d9f3efcf0937c915abce4693f51a4001acc9242f
Julian Köhler <julkoehl@icloud.com>
2023-12-06 21:46:21 +0100

    cors adjustment

--- app/main.py
+++ app/main.py
@@ -12,8 +12,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
+    expose_headers=['*'],
 )
 
 app.include_router(users.router)
